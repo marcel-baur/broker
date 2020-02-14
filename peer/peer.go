c95f82acd74869f498c2065d4f0b53920622325b
Marcel Baur <mail@marcelbaur.io>
2020-02-14 09:41:34 +0100

    Implement concurrent tcp connection on peer

---
+++ peer/peer.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Println("Please provide a port!")
+		return
+	}
+	PORT := ":" + args[1]
+	l, err := net.Listen("tcp4", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
+	rand.Seed(time.Now().Unix())
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		go handleConnection(c)
+	}
+}
+
+func handleConnection(c net.Conn) {
+	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		temp := strings.TrimSpace(string(netData))
+		// TODO: differentiate incoming requests
+		if temp == "STOP" {
+			c.Write([]byte("Shutting down peer..."))
+			break
+		} else {
+			result := strconv.Itoa(404) + "\n"
+			c.Write([]byte(string(result)))
+		}
+	}
+	c.Close()
+}
